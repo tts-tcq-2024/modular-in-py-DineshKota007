32cd0a96d6cfce65e5cd5962d61d1c0f755846d5
DineshKota007 <kotadsk@gmail.com>
2024-07-19 15:33:14 +0530

    Create TestRunnner.py

---
+++ TestRunnner.py
@@ -0,0 +1,5 @@
+from ColorToPairNumberTest import ColorToPairNumberTest
+from PairNumberToColorMapper import PairNumberToColorMapper
+def run_tests():
+    ColorToPairNumberTest()
+    PairNumberToColorMapper()
