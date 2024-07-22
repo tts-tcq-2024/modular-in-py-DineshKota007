d70573ce4c527644a1d3f908cdf59a1399990a2d
DineshKota007 <kotadsk@gmail.com>
2024-07-22 15:37:53 +0530

    Update ColorPair.py

--- ColorPair.py
+++ ColorPair.py
@@ -1,10 +1,9 @@
+from dataclasses import dataclass
+ 
+@dataclass
 class ColorPair:
-    def __init__(self, major_color, minor_color):
-        self.major_color = major_color
-        self.minor_color = minor_color
+    major_color: str
+    minor_color: str
  
     def __str__(self):
         return f"MajorColor: {self.major_color}, MinorColor: {self.minor_color}"
- 
-    def __eq__(self, other):
-        return self.major_color == other.major_color and self.minor_color == other.minor_color
