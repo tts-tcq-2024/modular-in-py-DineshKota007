b8e61a838b1083aefe0c01af5e64d21565efabf8
DineshKota007 <kotadsk@gmail.com>
2024-07-22 15:42:49 +0530

    Update TestRunner.py

--- TestRunner.py
+++ TestRunner.py
@@ -1,5 +1,29 @@
-from ColorToPairNumberMapperTest import ColorToPairNumberMapperTest
-from PairNumberToColorMapperTest import PairNumberToColorMapperTest
+from color_pair_mapper import ColorPairMapper
+from color_pair import ColorPair
 def run_tests():
-    ColorToPairNumberMapperTest()
-    PairNumberToColorMapperTest()
+    mapper = ColorPairMapper()
+    pair_number = 4
+    test_pair1 = mapper.map_number_to_color_pair(pair_number)
+    print(f"[In]Pair Number: {pair_number}, [Out] Colors: {test_pair1}")
+    assert test_pair1.major_color == 'White'
+    assert test_pair1.minor_color == 'Brown'
+    pair_number = 5
+    test_pair1 = mapper.map_number_to_color_pair(pair_number)
+    print(f"[In]Pair Number: {pair_number}, [Out] Colors: {test_pair1}")
+    assert test_pair1.major_color == 'White'
+    assert test_pair1.minor_color == 'SlateGray'
+    pair_number = 23
+    test_pair1 = mapper.map_number_to_color_pair(pair_number)
+    print(f"[In]Pair Number: {pair_number}, [Out] Colors: {test_pair1}")
+    assert test_pair1.major_color == 'Violet'
+    assert test_pair1.minor_color == 'Green'
+    test_pair2 = ColorPair(major_color='Yellow', minor_color='Green')
+    pair_number = mapper.map_color_pair_to_number(test_pair2)
+    print(f"[In]Colors: {test_pair2}, [Out] PairNumber: {pair_number}")
+    assert pair_number == 18
+    test_pair2 = ColorPair(major_color='Red', minor_color='Blue')
+    pair_number = mapper.map_color_pair_to_number(test_pair2)
+    print(f"[In]Colors: {test_pair2}, [Out] PairNumber: {pair_number}")
+    assert pair_number == 6
+if __name__ == "__main__":
+    run_tests()
